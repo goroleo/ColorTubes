6ba9487f34b0279cf6f4794dd8cb7388017cd43d
goroleo <legoru@me.com>
2025-01-24 22:54:09 +0300

    in progress

---
+++ gui/arrowitem.cpp
@@ -0,0 +1,6 @@
+#include "arrowitem.h"
+
+ArrowItem::ArrowItem()
+{
+
+}
---
+++ gui/arrowitem.h
@@ -0,0 +1,13 @@
+#ifndef ARROWITEM_H
+#define ARROWITEM_H
+
+#include <QQuickItem>
+
+class ArrowItem
+{
+    Q_OBJECT
+public:
+    ArrowItem();
+};
+
+#endif // ARROWITEM_H
